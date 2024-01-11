22838c37c3ed3ef52fd2afd982164680a619b6b8
Mario Dias <mariodias@Marios-MacBook-Pro.local>
2024-01-10 21:27:12 -0300

    [new] App: Including NavigationBarView

--- app/src/main/java/br/com/mario/yuugen/MainActivity.kt
+++ app/src/main/java/br/com/mario/yuugen/MainActivity.kt
@@ -2,10 +2,24 @@ package br.com.mario.yuugen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.mario.yuugen.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupBottomNavView()
+
+    }
+
+    private fun setupBottomNavView() {
+        binding.bottomNavView.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+        }
     }
 }
---
+++ app/src/main/java/br/com/mario/yuugen/YuugenApp.kt
@@ -0,0 +1,15 @@
+package br.com.mario.yuugen
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class YuugenApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
